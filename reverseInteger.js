f5b24820b4bb70197ad5fcfd903ecd7ee43f2975
github username <github email address>
2020-04-03 16:00:26 -0400

    save all

--- reverseInteger.js
+++ reverseInteger.js
@@ -1,3 +1,26 @@
+//way 1
+var reverse = function(x) {
+    let isNegative = Math.sign(x)
+    let min = Math.pow(-2,31)
+    let max = Math.pow(2,31) - 1
+    if(isNegative === 1){
+        result = parseInt(x.toString().split("").reverse().join(""))
+        if(result > min && result < max){
+            return result
+        }else{
+            return 0
+        }
+    }else{
+        result = parseInt(x.toString().split("").slice(1).reverse().join(""))
+        if(result > min && result < max){
+            return isNegative * result
+        }else{
+            return 0
+        }
+    }
+};
+
+// way 2
 var reverse = function(x) {
     const min = Math.pow(-2,31), max = Math.pow(2,31)-1  
     let reverseNum = parseInt(x.toString().split("").reverse().join(""), 10)

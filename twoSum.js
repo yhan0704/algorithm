edaa890335abe13ec11421ac7361b53053432715
github username <github email address>
2020-04-04 14:31:42 -0400

    done

--- twoSum.js
+++ twoSum.js
@@ -1,4 +1,4 @@
-nums = [2, 11, 7, 15], target = 9
+nums = [2, 7, 11, 15], target = 9
 
 var twoSum = function(nums, target) { 
     let obj = {}
@@ -6,7 +6,6 @@ var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++){
         let pairNums = target - nums[i]
         obj[nums[i]] = i
-        console.log(obj[7])
         if(pairNums in obj){
             emptyArray.push(obj[pairNums], i)
             return emptyArray

acf0b8c42f110dd499ba1b8885d95784aa3a6394
yhan0704 <hanyc1205@gmail.com>
2020-10-29 15:30:22 -0400

    explain URLify

--- URLify.js
+++ URLify.js
@@ -1,3 +1,6 @@
+//Cracking Coding Interview
+
+//replace all spaces in a string with %20
 function URLify(str, count){
     let realString = str.slice(0, count)
     return realString.replaceAll(" ","%20")
